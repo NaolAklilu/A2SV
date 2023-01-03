c0a02b3a2fc18e8da312cd3a6e9f422fe6280f74
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-01-03 18:19:07 +0300

    Time: 1931 ms (19.09%), Space: 18.9 MB (55.14%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -6,12 +6,12 @@ class Solution:
             col = []
             for j in range(len(grid)):
                 col.append(grid[j][i])
-            columns.append(col)
+            columns.append(tuple(col))
            
         count = 0
         for row in range(len(grid)):
             for col in range(len(grid)):
-                if grid[row] == columns[col]:
+                if tuple(grid[row]) == columns[col]:
                     count += 1
                     
         return count
