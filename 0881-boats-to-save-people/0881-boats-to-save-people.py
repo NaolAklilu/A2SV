98752ffbad17fafa2a1aad2f1ffc678f2a82f77f
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-01-25 18:38:20 +0300

    Time: 455 ms (92.17%), Space: 20.8 MB (94.98%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -6,16 +6,10 @@ class Solution:
         
         count = 0
         while left <= right:
-            if left == right:
-                right -= 1
-            
-            else:
-                if people[right] == limit or people[right]+people[left] > limit:
-                    right -= 1
-                else:
-                    left += 1
-                    right -= 1
+            if people[right]+people[left] <= limit:
+                left += 1
                     
+            right -= 1
             count += 1
             
         return count
