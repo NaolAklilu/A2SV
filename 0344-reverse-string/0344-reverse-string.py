5402c6e01620e8edf652844af458fc637efa94dc
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-02-28 16:01:21 +0300

    Time: 213 ms (34.20%), Space: 43.2 MB (5.49%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,6 +1,20 @@
 class Solution:
+    def reverse_handler(self, left, right, s):
+        if left == right:
+            return s[0]
+        
+        s[left], s[right] = s[right], s[left]
+        
+        if left+1 > right-1:
+            return 
+        else:
+            return self.reverse_handler(left+1, right-1,s)
+        
     def reverseString(self, s: List[str]) -> None:
-        for i in range(len(s)//2):
-            s[i], s[len(s)-1-i] = s[len(s)-1-i], s[i]
-            
+        left = 0
+        right = len(s)-1
+        
+        self.reverse_handler(left, right, s)
+        
+           
         
