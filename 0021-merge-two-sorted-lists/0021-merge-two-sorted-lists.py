222f5eacad63318ad5b2cd3ea9348d3fd19c1134
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-02-28 22:47:58 +0300

    Time: 38 ms (71.39%), Space: 14 MB (23.39%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -4,30 +4,24 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    
+    def merge_helper(self, list1: Optional[ListNode], list2: Optional[ListNode]):
+        if list1 == None and list2 == None:
+            return None
+        elif list1 == None:
+            return list2
+        elif list2 == None:
+            return list1
+        elif list1.val <= list2.val:
+            list1.next = self.merge_helper(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.merge_helper(list1, list2.next)
+            return list2
+    
+    
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-
-        list1head = list1
-        list2head = list2
-        
         dummyNode = ListNode()
-        temp = dummyNode
+        dummyNode.next = self.merge_helper(list1, list2)
         
-        while list1head != None and list2head != None:
-            if list1head.val <= list2head.val:
-                temp.next = list1head
-                list1head = list1head.next
-            else:
-                temp.next = list2head
-                list2head = list2head.next
-                
-            temp = temp.next
-
-        
-        if list1head != None:
-            temp.next = list1head
-                
-        elif list2head != None:
-            temp.next = list2head
-            
-        head = dummyNode.next
-        return head
+        return dummyNode.next
