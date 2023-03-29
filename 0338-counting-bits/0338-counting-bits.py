d2cee2b8270cb37f05e35e26cb076f0416c676b0
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-03-29 18:49:40 +0300

    Time: 749 ms (5.02%), Space: 20.8 MB (19.07%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -5,7 +5,7 @@ class Solution:
         for i in range(n+1):
             count = 0
             for j in range(32):
-                if (i >> j) & 1:
+                if i & 2**j:
                     count += 1
             ans[i] = count
             
