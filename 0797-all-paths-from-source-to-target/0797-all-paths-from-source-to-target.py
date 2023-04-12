c797ad7bde225abdddbb029379ec61d901793712
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-04-12 19:30:35 +0300

    Time: 102 ms (62.55%), Space: 15.8 MB (34.39%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,17 +1,12 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        
-        ans = []
-        # adjacencyList = defaultdict(list)
-        # for node1, node2 in graph:
-        #     adjacencyList[node1].append(node2)
-            
+        paths = []            
             
         def pathFinding(node, arr):
             curArray = arr[:]
             curArray.append(node)
             if node == len(graph)-1:
-                ans.append(curArray)
+                paths.append(curArray)
                 return
                 
             for neighbor in graph[node]:
@@ -19,9 +14,8 @@ class Solution:
             
             return
         
-        
         pathFinding(0, [])
-        return ans
+        return paths
             
                 
             
