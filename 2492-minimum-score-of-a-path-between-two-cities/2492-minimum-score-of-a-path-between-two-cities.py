15ecb844d74a475f77ecce42e4ab18408f07b0a8
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-05-18 21:29:16 +0300

    Time: 1906 ms (29.68%), Space: 65.8 MB (74.56%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,45 @@
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        parent = { node: node for node in range(1, n+1)}
+        size = { node: 1 for node in range(1, n+1)}
+        minimum = { node: float(inf) for node in range(1, n+1)}
+        
+        def find(node):
+            x = node
+            nodeParent = parent[x]
+            while parent[x] != x:
+                nodeParent = parent[x]
+                x = parent[x]
+                
+            while node != parent[node]:
+                parent[node] = nodeParent
+                node = parent[node]
+                
+            return nodeParent
+        
+        def union(node1, node2, cost):
+            node1Rep = find(node1) 
+            node2Rep = find(node2)
+            size1 = size[node1Rep]
+            size2 = size[node2Rep]
+            
+            if size1 >= size2:
+                parent[node2Rep] = node1Rep
+                size[node1Rep] += size2
+                size[node2Rep] 
+                minimum[node1Rep] = min(cost, min(minimum[node1Rep], minimum[node2Rep]))
+
+            else:
+                parent[node1Rep] = node2Rep
+                size[node2Rep] += size1
+                size[node1Rep] = 1
+                minimum[node2Rep] = min(cost, min(minimum[node1Rep], minimum[node2Rep]))
+
+            
+        for edge in roads:
+            union(edge[0], edge[1], edge[2])
+            
+        sourceParent = find(1)
+        destinationParent = find(n)
+            
+        return minimum[sourceParent]
