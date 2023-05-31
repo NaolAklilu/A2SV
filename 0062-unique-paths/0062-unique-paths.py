42358a478c6ec79767284bcb54889dc9157c0bfb
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-05-31 19:03:45 +0300

    Time: 51 ms (20.06%), Space: 16.3 MB (21.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,15 +1,21 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = defaultdict(tuple)
+        dp = [[0 for i in range(n)] for j in range(m)] 
+        dp[-1][-1] = 1
         
-        def findPath(x, y):
-            if x == n-1 or y == m-1:
-                return 1
+        def inBound(row, col):
+            if 0<= row < m and 0<= col < n:
+                return dp[row][col]
+            return 0
+        
+        for row in range(m-1, -1,-1):
+            for col in range(n-1, -1,-1):
+                if (row, col) == (m-1, n-1):
+                    continue
+                dp[row][col] = inBound(row+1, col) + inBound(row, col+1)
+               
+        return dp[0][0]
             
-            if (x, y) not in memo:
-                memo[(x, y)] = findPath(x+1, y) + findPath(x, y+1)
-                
-            return memo[(x, y)]
         
-        return findPath(0, 0)
+        
             
