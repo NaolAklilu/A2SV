44f88603fe917eb8e90857a954b24b684c087966
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-05-31 18:35:44 +0300

    Time: 43 ms (59.42%), Space: 16.3 MB (30.86%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,15 +1,15 @@
 class Solution:
-    
-    def __init__(self):
-        self.memory = defaultdict(int)
-    
     def fib(self, n: int) -> int:
+        
         if n <= 1:
-            self.memory[n] = n
-            return self.memory[n]
+            return n
         
-        self.memory[n] = self.fib(n-1) + self.fib(n-2)
+        dp = [0 for i in range(n+1)]
+        dp[0], dp[1] = 0 , 1
         
-        return self.memory[n]
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+            
+        return dp[n]
     
     
