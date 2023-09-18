579f05b43b9a9f930ede97b422cf498acecde2f1
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-09-18 11:04:28 +0300

    Time: 1263 ms (8.20%), Space: 243.4 MB (13.83%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,10 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        # Top-Down approach
-        @lru_cache(None)  # Does caching which avoid repeated computation
+        # Top-Down approach        
+        memo = defaultdict(int)
+        
         def dp(pos, bought):
             if pos == len(prices):
                 return 0
+            
+            if (pos, bought) in memo:
+                return memo[(pos, bought)]
             
             max_price = 0
             if not bought:
@@ -14,6 +18,7 @@ class Solution:
                 
             max_price = max(max_price, dp(pos+1, bought))
             
+            memo[(pos, bought)] = max_price
             return max_price
             
         return dp(0, False)
