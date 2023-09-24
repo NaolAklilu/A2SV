b3588140a4c6147cd308981d882c78aa9fef13f5
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-09-24 21:44:55 +0300

    Time: 43 ms (76.41%), Space: 16.1 MB (98.69%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,21 +1,20 @@
 class Solution:
     def minSteps(self, n: int) -> int:
         
-        if n == 1:
+        if n==1:
             return 0
         
-        def dp(curLength, curCopied, operationCount):
-            if curLength == n:
-                return operationCount
-            
-            elif curLength > n:
-                return float(inf)
-            
-            else:
-                operation1 = dp(curLength+curLength, curLength, operationCount+2)
-                operation2 = dp(curLength+curCopied, curCopied, operationCount+1)
-            
-            return min(operation1, operation2)
+        count = 0
+        i = 2
         
-        return dp(1, 1, 1)
+        while i**2 <= n:
+            if n%i == 0:
+                count += i
+                n /= i
+            else:
+                i += 1
+                
+        return int(count + n)
+        
+        
         
