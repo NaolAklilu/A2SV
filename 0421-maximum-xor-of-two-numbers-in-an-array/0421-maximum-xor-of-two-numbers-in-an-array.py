658f948fda36f2e6e3fe675ab77771214b48483a
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-09-29 14:34:42 +0300

    Time: 6438 ms (9.88%), Space: 635.2 MB (44.37%) - LeetHub

---
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.py
@@ -0,0 +1,58 @@
+# class TrieNode:
+#     def __init__(self, val):
+#         self.val = val
+#         self.children = {}
+#         self.num = -1
+
+class Solution:
+    def __init__(self):
+        self.root = {}
+    
+    def insert(self, num):
+        cur = self.root
+        curNum = "{:032b}".format(num)
+        
+        for char in curNum:
+            if char not in cur:
+                newNode = {}
+                cur[char] = newNode
+            
+            cur = cur[char]
+    
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        for num in nums:
+            self.insert(num)
+
+        maxNum = 0
+        for num in nums:
+            cur = self.root
+            curNum = "{:032b}".format(num)
+            
+            curBit = []
+            for bit in curNum:
+                if bit == "0":
+                    if "1" in cur:
+                        cur = cur["1"]
+                        curBit.append("1")
+                    else:
+                        cur = cur[bit]
+                        curBit.append(bit)
+                else:
+                    if "0" in cur:
+                        cur = cur["0"]
+                        curBit.append("0")
+                    else:
+                        cur = cur[bit]
+                        curBit.append(bit)
+            
+            pairNum = 0
+            for i in range(32):
+                if curBit[i] == '1':
+                    pairNum += 2**(32-i-1)
+            
+            maxNum = max(maxNum, num^pairNum)
+        
+        return maxNum
+    
+    
+        
