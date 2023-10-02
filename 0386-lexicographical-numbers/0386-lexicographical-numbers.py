627563a0315a1c33df2da0a7ac1f450f18d4e466
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-10-02 23:04:32 +0300

    Time: 312 ms (5.02%), Space: 35.5 MB (5.02%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,9 +1,46 @@
+class TrieNode:
+    
+    def __init__(self, val):
+        self.val = val
+        self.children = {}
+        self.is_end = False
+        self.number = -1
+
 class Solution:
-    def lexicalOrder(self, n: int) -> List[int]:
-        nums = [str(i) for i in range(1, n+1)]
-        nums.sort()
-        for i in range(n):
-            nums[i] = int(nums[i])
+    
+    def __init__(self):
+        self.root = TrieNode(".")
+        
+    def insert(self, num):
+        cur = self.root
+        digits = list(num)
+        
+        for digit in digits:
+            if digit not in cur.children:
+                cur.children[digit] = TrieNode(digit)
             
-        return nums
+            cur = cur.children[digit]
+            
+        cur.is_end = True
+        cur.number = num
+        
+    def dfs(self, curNode, result):
+        if curNode.is_end == True:
+            result.append(curNode.number)
+        
+        for n in range(10):
+            if str(n) in curNode.children:
+                self.dfs(curNode.children[str(n)], result)
+    
+    def lexicalOrder(self, n: int) -> List[int]:
+        for num in range(1, n+1):
+            self.insert(str(num))
+            
+        cur = self.root
+        result = []
+        self.dfs(cur, result)
+        
+        return [int(num) for num in result]
+        
+        
             
