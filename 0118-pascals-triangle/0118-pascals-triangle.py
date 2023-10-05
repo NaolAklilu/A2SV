494a2ad409740001503f429eedfb5d14240e096b
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-10-05 23:04:19 +0300

    Time: 41 ms (51.95%), Space: 16.6 MB (30.58%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,24 +1,39 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        triangle = [[1]]
+        triangle = {}
         
-        for row in range(2, numRows+1):
-            curRow = []
-            for index in range(row):
-                left = index - 1
-                right = index
-                curValue = 0
-                if left >= 0:
-                    curValue += triangle[row-2][left]
-                
-                if right < len(triangle[row-2]):
-                    curValue += triangle[row-2][right]
-                
-                curRow.append(curValue)
+        def dfs(level, row, col):
+            if row-1 < 0 and col-1 < 0:
+                triangle[(row, col)] = 1
+                return triangle[(row, col)]
             
-            triangle.append(curRow)
+            left, right = 0, 0
+            if row-1 >= 0 and col-1 >= 0:
+                if (row-1, col-1) not in triangle:
+                    left += dfs(level-1, row-1, col-1)
+                else:
+                    left += triangle[(row-1, col-1)]
             
+            if col < level-1 and col >= 0:
+                if (row-1, col) not in triangle:
+                    right += dfs(level-1, row-1, col)
+                else:
+                    right += traingle[(row-1, col)]
+            
+            triangle[(row, col)] = left+right
+            
+            return triangle[(row, col)]
         
-        return triangle
+        for i in range(numRows):
+            dfs(numRows, numRows-1, i)
+
+        result = [[] for _ in range(numRows)]
+        for i in range(numRows):
+            for k in range(i+1):
+                result[i].append(triangle[(i, k)])                
+        
+        return result
+        
+        
                     
         
