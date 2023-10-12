0d0e1186395668c08a2a8e54f01cc16db984d16c
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-10-12 14:27:57 +0300

    Time: 36 ms (75.20%), Space: 16.3 MB (67.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,21 +1,20 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0 for i in range(n)] for j in range(m)] 
-        dp[-1][-1] = 1
         
-        def inBound(row, col):
-            if 0<= row < m and 0<= col < n:
-                return dp[row][col]
-            return 0
+        memo = {}
         
-        for row in range(m-1, -1,-1):
-            for col in range(n-1, -1,-1):
-                if (row, col) == (m-1, n-1):
-                    continue
-                dp[row][col] = inBound(row+1, col) + inBound(row, col+1)
-               
-        return dp[0][0]
+        def factorial(num):
+            if num <= 1:
+                return 1
             
-        
-        
+            if num not in memo:
+                memo[num] = num*factorial(num-1)
             
+            return memo[num]
+        
+        total = m-1 + n-1
+        
+        return factorial(total)//(factorial(m-1)*factorial(n-1))
+        
+        
+        
