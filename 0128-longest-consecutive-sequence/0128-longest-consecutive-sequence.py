8866b3c53bdf9ac01f181aa292b2b36b2d8697f5
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-10-18 22:48:54 +0300

    Time: 711 ms (37.32%), Space: 55.1 MB (5.03%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -3,20 +3,42 @@ class Solution:
         if len(nums) <= 1:
             return len(nums)
         
-        maxCount = 0
-        nums.sort()
+        nums_set = set(nums)
+        parent = {num:num for num in nums_set}
+        rank = {num:1 for num in nums_set}
         
-        curCount = 1
-        for i in range(1, len(nums)):
-            if nums[i] - nums[i-1] == 1:
-                curCount += 1
-            else:
-                if nums[i] == nums[i-1]:
-                    continue
-                maxCount = max(curCount, maxCount)
-                curCount = 1
+        def find(num):
+            if num != parent[num]:
+                parent[num] = find(parent[num])
+            
+            return parent[num]
+        
+        def combine(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            
+            if p1 != p2:
+                if rank[p1] >= rank[p2]:
+                    parent[p2] = p1
+                    rank[p1] += rank[p2]
+                else:
+                    parent[p1] = p2
+                    rank[p2] += rank[p1]
+            
+        for num in nums_set:
+            if num+1 in nums_set:
+                combine(num+1, num)
+            if num-1 in nums_set:
+                combine(num-1, num)
+        
+        results = defaultdict(list)
+        for num in nums_set:
+            results[find(num)].append(num)
+            
+        maxSet = 0
+        for st in results.values():
+            maxSet = max(maxSet, len(st))
+            
+        return maxSet 
+        
                 
-        maxCount = max(maxCount, curCount)
-        
-        return maxCount
             
