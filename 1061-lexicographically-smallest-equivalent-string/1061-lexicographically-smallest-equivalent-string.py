e6c67288b2fe1666d46b87eed6e3c3d1d48385f7
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-10-19 17:56:34 +0300

    Time: 38 ms (94.29%), Space: 16.5 MB (12.66%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -1,48 +1,45 @@
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        parent = {}
-        size = {}
-        for char in s1:
-            parent[char] = char
-            size[char] = 1
-            
-        for char in s2:
-            parent[char] = char
-            size[char] = 1
-            
+        parent = defaultdict(str)
+        rank = defaultdict(int)
+        
         def find(char):
-            if parent[char] == char:
-                return char
-            
-            parent[char] = find(parent[char])
+            if char != parent[char]:
+                parent[char] = find(parent[char])
+                
             return parent[char]
             
-        def union(char1, char2):
-            parent1 = find(char1)
-            parent2 = find(char2)
-
-            if ord(parent1) < ord(parent2):
-                parent[parent2] = parent1
-                for char in parent:
-                    if parent[char] == parent2:
-                        parent[char] = parent1
-            else:
-                parent[parent1] = parent2
-                for char in parent:
-                    if parent[char] == parent1:
-                        parent[char] = parent2
+            
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            
+            if p1 != p2:
+                if ord(p1) <= ord(p2):
+                    parent[p2] = p1
+                    rank[p1] += rank[p2]
+                else:
+                    parent[p1] = p2
+                    rank[p2] += rank[p1]
+                    
+        for i in range(len(s1)):
+            parent[s1[i]] = s1[i]
+            parent[s2[i]] = s2[i]
+            rank[s1[i]] = 1
+            rank[s2[i]] = 1
+            
+        for i in range(len(baseStr)):
+            parent[baseStr[i]] = baseStr[i]
+            rank[baseStr[i]] = 1
             
         for i in range(len(s1)):
             union(s1[i], s2[i])
+            
+        ans = []
+        for i in range(len(baseStr)):
+            ans.append(find(baseStr[i]))
         
-        result = []
-        for char in baseStr:
-            if char in parent:
-                result.append(parent[char])
-            else:
-                result.append(char)
+        return "".join(ans)
                 
-        return "".join(result)
-            
-            
+                
+                
             
