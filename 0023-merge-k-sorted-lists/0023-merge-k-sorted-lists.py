7384950877e38c811a13284eb3710a3b3581a2e3
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2024-03-15 15:02:16 +0300

    Time: 81 ms (49.22%), Space: 19.6 MB (48.93%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,24 +5,30 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        dummyNode = ListNode()
-        
-        heap = []
-        head = ListNode()
-        for linkedList in lists:
-            while linkedList:
-                head.next = linkedList
-                heap.append(head.val)
-                linkedList = linkedList.next
-                head = head.next
-        heap.append(head.val)
-        heappop(heap)
-        heapify(heap)
+        # Initialize a heap with the first node of each list
+        heap = [(node.val, idx) for idx, node in enumerate(lists) if node]
+        heapq.heapify(heap)
 
-        temp = dummyNode
+        # Initialize the head of the result list
+        head = point = ListNode()
+
         while heap:
-            num = heappop(heap)
-            temp.next = ListNode(val=num)
-            temp = temp.next
+            val, idx = heapq.heappop(heap)
+            point.next = ListNode(val)
+            point = point.next
+
+            # If there is a next node in the list, add it to the heap
+            if lists[idx].next:
+                heapq.heappush(heap, (lists[idx].next.val, idx))
+                lists[idx] = lists[idx].next
+
+        return head.next
         
-        return dummyNode.next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+    
+        
