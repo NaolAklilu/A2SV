05a15adaaedc0a1a746ada41606e9f9802dc7cd9
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2024-03-15 14:33:12 +0300

    Time: 41 ms (25.87%), Space: 16.6 MB (61.88%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,39 +1,12 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        triangle = {}
-        
-        def dfs(level, row, col):
-            if row-1 < 0 and col-1 < 0:
-                triangle[(row, col)] = 1
-                return triangle[(row, col)]
-            
-            left, right = 0, 0
-            if row-1 >= 0 and col-1 >= 0:
-                if (row-1, col-1) not in triangle:
-                    left += dfs(level-1, row-1, col-1)
-                else:
-                    left += triangle[(row-1, col-1)]
-            
-            if col < level-1 and col >= 0:
-                if (row-1, col) not in triangle:
-                    right += dfs(level-1, row-1, col)
-                else:
-                    right += traingle[(row-1, col)]
-            
-            triangle[(row, col)] = left+right
-            
-            return triangle[(row, col)]
-        
+      
+        result = []
         for i in range(numRows):
-            dfs(numRows, numRows-1, i)
-
-        result = [[] for _ in range(numRows)]
-        for i in range(numRows):
-            for k in range(i+1):
-                result[i].append(triangle[(i, k)])                
-        
+            row = [1]
+            if result:
+                last_row = result[-1]
+                row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+                row.append(1)
+            result.append(row)
         return result
-        
-        
-                    
-        
