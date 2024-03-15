881840dac190250b343f7a0f1c0c7dc6fb3cd1fb
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2024-03-15 16:32:06 +0300

    Time: 188 ms (45.98%), Space: 17.7 MB (32.60%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,32 +1,18 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        
+        def dfs(node):
+            for nei, val in enumerate(isConnected[node]):
+                if val and nei not in visited:
+                    visited.add(nei)
+                    dfs(nei)
+
         n = len(isConnected)
-        parent = {num:num for num in range(1, n+1)}
-        rank = {num:1 for num in range(1, n+1)}
-        
-        def find(num):
-            if num != parent[num]:
-                parent[num] = find(parent[num])
-            return parent[num]
-        
-        def union(n1, n2):
-            p1, p2 = find(n1), find(n2)
-            
-            if p1 != p2:
-                if rank[p1] >= rank[p2]:
-                    parent[p2] = p1
-                    rank[p1] += rank[p2]
-                else:
-                    parent[p1] = p2
-                    rank[p2] += rank[p1] 
-            
-        for row in range(n):
-            for col in range(n):
-                if isConnected[row][col] == 1:
-                    union(row+1, col+1)
-                    
-        parents = set()
-        for num in parent:
-            parents.add(find(num))
-        
-        return len(parents)
+        visited = set()
+        provinces = 0
+        for i in range(n):
+            if i not in visited:
+                visited.add(i)
+                dfs(i)
+                provinces += 1
+        return provinces
