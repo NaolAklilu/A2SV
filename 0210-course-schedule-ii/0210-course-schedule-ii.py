6618ca54e3278bacb1aac8d578c6b2a235646004
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2024-03-15 14:16:59 +0300

    Time: 78 ms (98.18%), Space: 18.1 MB (97.57%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,30 +1,26 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = defaultdict(list)
-        indegree = [0 for _ in range(numCourses)]
-        orders = []
-        
-        for course, pre in prerequisites:
-            graph[pre].append(course)
-            indegree[course] += 1
-            
-        queue = deque()
-        for course in range(numCourses):
-            if indegree[course] == 0:
-                queue.append(course)
-                
-        while queue:
-            curCourse = queue.popleft()
-            orders.append(curCourse)
-            
-            for pre in graph[curCourse]:
-                indegree[pre] -= 1
-                
-                if indegree[pre] == 0:
-                    queue.append(pre)
-                    
-        if len(orders) != numCourses:
-            return []
-        return orders
-        
-        
+        graph = [[] for _ in range(numCourses)]
+        visit = [0 for _ in range(numCourses)]
+        res = []
+
+        for x, y in prerequisites:
+            graph[x].append(y)
+
+        def dfs(i):
+            if visit[i] == -1:
+                return False
+            if visit[i] == 1:
+                return True
+            visit[i] = -1
+            for j in graph[i]:
+                if not dfs(j):
+                    return False
+            visit[i] = 1
+            res.append(i)
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+        return res
