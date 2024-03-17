f9d17452765cc39968fe99120124b48408eac04a
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2024-03-17 10:31:18 +0300

    Time: 98 ms (18.73%), Space: 22 MB (15.84%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,21 +1,26 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
             # Count the frequency of each element in the array
-        freq_counter = collections.Counter(nums)
-
-        # Create a min-heap
-        min_heap = []
-
-        # Add each element and its frequency to the heap
-        for num, freq in freq_counter.items():
-            # If the heap is full, check if the current frequency is greater than the smallest frequency in the heap
-            if len(min_heap) == k:
-                if freq > min_heap[0][0]:
-                    # Remove the smallest frequency and add the current frequency
-                    heapq.heapreplace(min_heap, (freq, num))
+        freq_counter = {}
+        for num in nums:
+            if num in freq_counter:
+                freq_counter[num] += 1
             else:
-                # Add the frequency and element to the heap
-                heapq.heappush(min_heap, (freq, num))
+                freq_counter[num] = 1
 
-        # Return the k most frequent elements
-        return [x[1] for x in min_heap]
+        # Create a list of buckets to store elements based on their frequency
+        buckets = [[] for _ in range(len(nums) + 1)]
+
+        # Add each element to its corresponding bucket
+        for num, freq in freq_counter.items():
+            buckets[freq].append(num)
+
+        # Get the k most frequent elements
+        result = []
+        for i in range(len(buckets) - 1, -1, -1):
+            for num in buckets[i]:
+                result.append(num)
+                if len(result) == k:
+                    return result
+
+        return result
