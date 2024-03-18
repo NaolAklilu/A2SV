94b2dfad2bdd7fd2c3adc402835929c9e5a04f59
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2024-03-18 16:01:44 +0300

    Time: 40 ms (19.70%), Space: 16.4 MB (98.54%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,8 +1,15 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        if n == 0:
-            return 1
-        G = 1
-        for i in range(n):
-            G = G * (4 * i + 2) // (i + 2)
-        return G
+        def helper(n, memo):
+            if n in memo:
+                return memo[n]
+            if n <= 1:
+                return 1
+            total = 0
+            for i in range(1, n+1):
+                total += helper(i-1, memo) * helper(n-i, memo)
+            memo[n] = total
+            return total
+
+        memo = {}
+        return helper(n, memo)
