ec6c1e63323f185fa4ad40df76af34f215eeb60d
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2024-03-20 16:57:58 +0300

    Time: 733 ms (82.94%), Space: 16.9 MB (61.82%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,24 +1,8 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        
-        @cache
-        def dp(index, curAmount):
-            
-            if index == len(coins):
-                if curAmount == 0:
-                    return 0            
-                return float('inf')
-            
-            notTake = 0 + dp(index+1, curAmount)
-            
-            take = float('inf')
-            if coins[index] <= curAmount:
-                take = 1 + dp(index, curAmount - coins[index])
-            
-            return min(notTake, take)
-        
-        result = dp(0, amount)
-        if result == float('inf'):
-            return -1
-        return result
-            
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+        return -1 if dp[amount] == float('inf') else dp[amount]
