456dd7dfe890888da61b088021536b95c6a4d732
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2024-03-20 16:59:09 +0300

    Time: 376 ms (98.20%), Space: 16.8 MB (91.02%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,8 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        # Create a table to store the minimum number of coins needed for each amount
         dp = [float('inf')] * (amount + 1)
-        dp[0] = 0
+        dp[0] = 0  # We can make up 0 amount with 0 coins
+
+        # For each coin in the array
         for coin in coins:
-            for i in range(coin, amount + 1):
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-        return -1 if dp[amount] == float('inf') else dp[amount]
+            # For each amount from the coin value to the target amount
+            for x in range(coin, amount + 1):
+                # If making up this amount with this coin is less than making up the amount without this coin
+                if dp[x - coin] + 1 < dp[x]:
+                    # Update the table
+                    dp[x] = dp[x - coin] + 1
+
+        # If we can't make up the target amount, return -1
+        if dp[amount] == float('inf'):
+            return -1
+        else:
+            return dp[amount]
