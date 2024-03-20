a7e2dd5c89656a569b99089b753297deda757be1
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2024-03-20 17:30:18 +0300

    Time: 54 ms (22.70%), Space: 16.7 MB (57.49%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -1,12 +1,17 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        dp = [0] * (days[-1] + 1)
-        for i in range(days[-1] + 1):
-            if i not in days:
-                dp[i] = dp[i-1]
-            else:
-                one_day = dp[i-1] + costs[0] if i-1 >= 0 else costs[0]
-                seven_days = dp[i-7] + costs[1] if i-7 >= 0 else costs[1]
-                thirty_days = dp[i-30] + costs[2] if i-30 >= 0 else costs[2]
-                dp[i] = min(one_day, seven_days, thirty_days)
-        return dp[-1]
+        def dp(day, memo):
+            if day < 0:
+                return 0
+            if day in memo:
+                return memo[day]
+            if day not in days:
+                memo[day] = dp(day - 1, memo)
+                return memo[day]
+            one_day = dp(day - 1, memo) + costs[0]
+            seven_days = dp(day - 7, memo) + costs[1]
+            thirty_days = dp(day - 30, memo) + costs[2]
+            memo[day] = min(one_day, seven_days, thirty_days)
+            return memo[day]
+
+        return dp(days[-1], {})
