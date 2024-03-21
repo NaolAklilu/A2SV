36a10554357902c1b64afc75651ef8d33fc3b4df
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2024-03-21 10:16:34 +0300

    Time: 138 ms (83.11%), Space: 16.8 MB (73.88%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,21 +1,10 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        
-        memo = defaultdict(tuple)
-        
-        def findTarget(i, curSum):
-            if i == len(nums):
-                if curSum == target:
-                    return 1
-                return 0
-            
-            if (i, curSum) not in memo:
-                memo[(i, curSum)] = findTarget(i+1, curSum + nums[i]) + findTarget(i+1, curSum-nums[i])
-            
-            return memo[(i, curSum)]
-     
-        return findTarget(0, 0)
-        
-            
-            
-            
+        total = {0: 1}
+        for num in nums:
+            newTotal = {}
+            for curNum, curCount in total.items():
+                newTotal[curNum + num] = newTotal.get(curNum + num, 0) + curCount
+                newTotal[curNum - num] = newTotal.get(curNum - num, 0) + curCount
+            total = newTotal
+        return total.get(target, 0)
