3b29f9b6f206c96ae73b37490758262588319d03
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2024-03-21 15:57:24 +0300

    Time: 1682 ms (50.26%), Space: 258.9 MB (39.45%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.py
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.py
@@ -1,36 +1,34 @@
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        results = set()
+        memo = {}
         pairs = []
-        
+
         for string in strs:
             oneCount = 0
             zeroCount = 0
-            
+
             for char in string:
                 if char == '0':
                     zeroCount += 1
                 else:
                     oneCount += 1
-                
+
             pairs.append((zeroCount, oneCount))
-        
-        @cache
-        def dp(index, count, zeroCount, oneCount):
+
+        def dp(index, curPair):
             if index == len(strs):
-                if zeroCount <= m and oneCount <= n:
-                    results.add(count)
-                return
-            
-            dp(index+1, count, zeroCount, oneCount)
-            
-            if zeroCount <= m and oneCount <= n:
-                count += 1
-                dp(index+1, count, zeroCount + pairs[index][0], oneCount + pairs[index][1])
-                  
-            return
-        
-        dp(0, 0, 0, 0)
-        return max(results)
-        
-            
+                return 0
+
+            if (index, curPair) in memo:
+                return memo[(index, curPair)]
+
+            include = 0
+            if curPair[0] + pairs[index][0] <= m and curPair[1] + pairs[index][1] <= n:
+                include = 1 + dp(index + 1, (curPair[0] + pairs[index][0], curPair[1] + pairs[index][1]))
+
+            exclude = dp(index + 1, curPair)
+
+            memo[(index, curPair)] = max(include, exclude)
+            return memo[(index, curPair)]
+
+        return dp(0, (0, 0))
