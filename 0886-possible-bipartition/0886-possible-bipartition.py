7b1d95f39b3c3cb684d89cde8112fb0e17c09def
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2024-03-22 23:57:03 +0300

    Time: 520 ms (77.08%), Space: 18.6 MB (26.04%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -5,34 +5,24 @@ class Solution(object):
         :type dislikes: List[List[int]]
         :rtype: bool
         """
-        
-        
-        groups = [-1 for i in range(n)]
-        groups[0] = 1
-        
-        graph = defaultdict(list)
-        for node1, node2 in dislikes:
-            graph[node1].append(node2)
-            graph[node2].append(node1)
-            
-        visited = [False for i in range(n)]
-        
-        def bipartite(node):
-            visited[node-1] = True
+        graph = [[] for _ in range(n+1)]
+        colors = [0 for _ in range(n+1)]
+
+        for dislike in dislikes:
+            graph[dislike[0]].append(dislike[1])
+            graph[dislike[1]].append(dislike[0])
+
+        def dfs(node, color):
+            colors[node] = color
             for neighbor in graph[node]:
-                if not visited[neighbor-1]:
-                    groups[neighbor-1] = 1 - groups[node-1]
-                    if not bipartite(neighbor):
-                        return False
-                else:
-                    if groups[neighbor-1] == groups[node-1]:
-                        return False
-            return True
-        
-        for i in range(1,n+1):
-            if not visited[i-1]:
-                if not bipartite(i):
+                if colors[neighbor] == color:
                     return False
-            
+                if colors[neighbor] == 0 and not dfs(neighbor, -color):
+                    return False
+            return True
+
+        for node in range(1, n+1):
+            if colors[node] == 0 and not dfs(node, 1):
+                return False
+
         return True
-        
