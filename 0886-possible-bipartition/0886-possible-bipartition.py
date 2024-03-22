2b51f88655e2c6eaa4711603d9c9148028e235af
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2024-03-22 23:34:48 +0300

    Time: 534 ms (56.25%), Space: 19.4 MB (18.75%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,5 +1,11 @@
-class Solution:
-    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+class Solution(object):
+    def possibleBipartition(self, n, dislikes):
+        """
+        :type n: int
+        :type dislikes: List[List[int]]
+        :rtype: bool
+        """
+        
         
         groups = [-1 for i in range(n)]
         groups[0] = 1
@@ -9,32 +15,24 @@ class Solution:
             graph[node1].append(node2)
             graph[node2].append(node1)
             
+        visited = [False for i in range(n)]
         
         def bipartite(node):
+            visited[node-1] = True
             for neighbor in graph[node]:
-                if groups[neighbor-1] == -1:
+                if not visited[neighbor-1]:
                     groups[neighbor-1] = 1 - groups[node-1]
-                    bipartite(neighbor)
-                
+                    if not bipartite(neighbor):
+                        return False
                 else:
                     if groups[neighbor-1] == groups[node-1]:
                         return False
-                    
-                
-                        
             return True
         
         for i in range(1,n+1):
-            result = bipartite(i)
-            if result == False:
-                return False
+            if not visited[i-1]:
+                if not bipartite(i):
+                    return False
             
         return True
-            
-                    
         
-         
-            
-        
-        
-        return True
