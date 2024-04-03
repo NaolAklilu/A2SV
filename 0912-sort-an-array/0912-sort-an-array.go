59dfc2c610be3e8d0c591af5613ffeadcd6966ce
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2024-04-03 15:42:28 +0300

    Time: 106 ms (78.62%), Space: 8.7 MB (47.54%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.go
@@ -0,0 +1,47 @@
+func mergeSort(nums []int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+
+	mid := len(nums) / 2
+	left := mergeSort(nums[:mid])
+	right := mergeSort(nums[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := make([]int, len(left)+len(right))
+	i := 0
+	j := 0
+	k := 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			result[k] = left[i]
+			i++
+		} else {
+			result[k] = right[j]
+			j++
+		}
+		k++
+	}
+
+	for i < len(left) {
+		result[k] = left[i]
+		i++
+		k++
+	}
+
+	for j < len(right) {
+		result[k] = right[j]
+		j++
+		k++
+	}
+
+	return result
+}
+
+func sortArray(nums []int) []int {
+   return mergeSort(nums) 
+}
